56115f52050b9156e63ab570674d16d54e1731ed
William Bos <wpnbos@gmail.com>
2021-06-13 22:49:47 +0200

    Sunday push. it's called dedication

--- utils.py
+++ utils.py
@@ -1,15 +1,3 @@
-import torch
-
-def init_device():
-  if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("There are %d GPU(s) available." % torch.cuda.device_count())
-    print("Using GPU: ", torch.cuda.get_device_name(0))
-
-  else:
-    print("No GPU available, using CPU instead.")
-    device = torch.device("cpu")
-
 class Node: 
     """Representation of a trie node"""
     __slots__ = ('children', 'value')
